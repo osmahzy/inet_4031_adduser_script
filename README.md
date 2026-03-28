60c0a1583ea0b626d42fbd7d63048ddf0ad3089c
Mahad Osman <osman435@umn.edu>
2026-03-28 16:50:00 +0000

    Add comments to script and update README

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # inet_4031_adduser_script
+
+## Description
+This Python script automates the creation of Linux user accounts and group
+assignments on an Ubuntu server. Instead of manually running adduser commands
+for each user, this script reads from a structured input file and processes
+each user automatically. This is useful when setting up multiple servers that
+need the same set of user accounts.
+
+## Files
+- **create-users.py** - The main Python script that creates users and assigns groups
+- **create-users.input** - The input file containing the list of users to create
+
+## Input File Format
+The input file is a colon-delimited text file. Each line represents one user:
+```
+username:password:lastname:firstname:group(s)
+```
+
+- Lines starting with `#` are skipped (comments)
+- Use `-` in the groups field to indicate no group assignment
+- Lines with fewer than 5 fields are skipped automatically
+- Multiple groups are comma-separated: `group01,group02`
+
+## How to Run
+
+**Step 1 - Make the script executable:**
+```
+chmod +x create-users.py
+```
+
+**Step 2 - Dry run (no users actually created):**
+
+Comment out the `os.system(cmd)` and `print(cmd)` lines in the script,
+then run:
+```
+./create-users.py < create-users.input
+```
+
+**Step 3 - Run for real:**
+```
+sudo ./create-users.py < create-users.input
+```
--- create-users.py
+++ create-users.py
@@ -3,51 +3,66 @@
 # Mahad Osman
 # Date Created: March 28, 2026
 # Date Last Modified: March 28, 2026
-# This script automates the creation of user accounts and group assignments
-# on a Linux system by reading from an input file.
+# This script automates the creation of Linux user accounts and group
+# assignments by reading from a colon-delimited input file via stdin.
+# Each line in the input file represents one user with fields:
+# username:password:lastname:firstname:group(s)
 
-import os       # Used to run operating system commands like adduser and passwd
-import re       # Used for regular expressions to detect comment/skip lines
-import sys      # Used to read input from stdin (the input file)
+# os module - used to run Linux system commands like adduser and passwd
+import os
+# re module - used for regular expressions to detect comment/skip lines
+import re
+# sys module - used to read input line by line from stdin
+import sys
 
 def main():
+    # Read the input file line by line from stdin
     for line in sys.stdin:
-        # Check if the line starts with '#' which marks it as a comment/skip line
+        # Check if the line starts with '#' which marks it as a comment
+        # Lines starting with '#' should be skipped entirely
         match = re.match("^#",line)
-        # Strip whitespace and split the line into fields using ':' as delimiter
+        # Remove leading/trailing whitespace and split the line into
+        # fields using ':' as the delimiter
         fields = line.strip().split(':')
-        # Skip the line if it is marked as a comment OR if it doesn't have
-        # exactly 5 fields - this handles bad/incomplete lines in the input file
+        # Skip the line if it is a comment (starts with #) OR if it does
+        # not have exactly 5 fields - this handles malformed/incomplete lines
+        # in the input file without crashing the script
         if match or len(fields) != 5:
             continue
-        # Extract username, password, and GECOS field (full name) from the fields
-        # GECOS format matches what is stored in /etc/passwd
+        # Extract user data from the parsed fields
+        # username - the Linux login name
+        # password - the user's password
+        # gecos - full name field stored in /etc/passwd (firstname lastname)
         username = fields[0]
         password = fields[1]
         gecos = "%s %s,,," % (fields[3],fields[2])
-        # Split the groups field by comma to get individual group names
+        # Split the groups field by comma to handle multiple group assignments
+        # A '-' in this field means no group assignment for this user
         groups = fields[4].split(',')
-        # Notify that the account creation process is starting for this user
+        # Notify that account creation is starting for this user
         print("==> Creating account for %s..." % (username))
-        # Build the adduser command with GECOS info and username
+        # Build the adduser command using the gecos info and username
+        # --disabled-password skips interactive password prompt
+        # --gecos sets the full name field in /etc/passwd
         cmd = "/usr/sbin/adduser --disabled-password --gecos '%s' %s" % (gecos,username)
-        # Uncomment the lines below to actually run the command
-        #print(cmd)
-        #os.system(cmd)
+        print(cmd)
+        os.system(cmd)
         # Notify that the password is being set for this user
         print("==> Setting the password for %s..." % (username))
-        # Build the passwd command to set the user's password
+        # Build the passwd command using echo to pipe the password twice
+        # (once for new password, once for confirmation)
         cmd = "/bin/echo -ne '%s\n%s' | /usr/bin/sudo /usr/bin/passwd %s" % (password,password,username)
-        # Uncomment the lines below to actually run the command
-        #print(cmd)
-        #os.system(cmd)
+        print(cmd)
+        os.system(cmd)
+        # Loop through each group and assign the user if group is not '-'
         for group in groups:
-            # If group is not '-' (empty indicator), assign the user to that group
+            # '-' indicates no group assignment - skip it
+            # Otherwise assign the user to the specified group
             if group != '-':
                 print("==> Assigning %s to the %s group..." % (username,group))
                 cmd = "/usr/sbin/adduser %s %s" % (username,group)
-                #print(cmd)
-                #os.system(cmd)
+                print(cmd)
+                os.system(cmd)
 
 if __name__ == '__main__':
     main()
